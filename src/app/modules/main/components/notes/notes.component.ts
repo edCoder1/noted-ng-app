5f6ab3c4e93e021f2ddd5b89813cb02ff153db9e
edCoder1 <edwin.lsb1@gmail.com>
2020-06-06 16:43:06 -0500

    notes.service v0.1

--- src/app/modules/main/components/notes/notes.component.ts
+++ src/app/modules/main/components/notes/notes.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from '../../services/notes.service';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.scss']
+  styleUrls: ['./notes.component.scss'],
 })
 export class NotesComponent implements OnInit {
+  constructor(private _notesService: NotesService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public getAllNotes(): any {
+    return this._notesService.getAllNotes().subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NotesComponent } from './components/notes/notes.component';
+import { NotesService } from './services/notes.service';
 
 const routes: Routes = [{ path: '', component: NotesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  providers: [NotesService],
   exports: [RouterModule],
 })
 export class MainRoutingModule {}
